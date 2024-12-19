4202bb544215a156e9f4b31f2717bcabd35d6eb2
Nour Ayman <norayman1234@gmail.com>
2024-12-19 05:11:34 +0200

    Improved dark mode UI

--- gaming-hub-main/static/js/darkmode.js
+++ gaming-hub-main/static/js/darkmode.js
@@ -1,14 +1,21 @@
 let darkmode = localStorage.getItem('darkmode')
 const themeSwitch = document.getElementById('theme-switch')
+const themeIcon = document.getElementById('theme-class-switch')
 
 const enableDarkmode = () => {
   document.body.classList.add('darkmode')
   localStorage.setItem('darkmode', 'active')
+
+  themeIcon.classList.remove('fa-sun');
+  themeIcon.classList.add('fa-moon');
 }
 
 const disableDarkmode = () => {
     document.body.classList.remove('darkmode')
     localStorage.setItem('darkmode', null)
+
+    themeIcon.classList.remove('fa-moon');
+    themeIcon.classList.add('fa-sun');
 }
 
 if (darkmode === "active") {
